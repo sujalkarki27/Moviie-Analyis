42a7f329c22acff364a27f147878fbf0502a2e9a
Sujal <sauravkarki51@gmail.com>
2025-12-24 19:21:44 +0530

    Your commit message here

---
+++ movie.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+
+
+
